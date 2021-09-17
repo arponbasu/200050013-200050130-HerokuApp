671c1884aa0042de807b5a3d219713308c29b706
arponbasu <200050013@iitb.ac.in>
2021-09-18 01:14:35 +0530

    Added configuration stuff to settings.py

--- cs251lab4_200050013_200050130/settings.py
+++ cs251lab4_200050013_200050130/settings.py
@@ -11,6 +11,7 @@ from pathlib import Path
 import os
 import sys
 import dj_database_url
+import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
